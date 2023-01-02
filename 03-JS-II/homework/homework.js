64e6c2b5d2711d71d6fb7698dd1c95adc4d5d1b7
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2023-01-02 20:13:17 -0300

    JS I to VI solved (2 hours)

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,14 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  return x > y ? x : y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  return edad >= 18 ? "Allowed" : "Not allowed";
 }
   
 function conection(status) {
@@ -19,6 +21,7 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  return status === 1 ? "Online" : status === 2 ? "Away" : "Offline";
 }
 
 function saludo(idioma) {
@@ -28,6 +31,7 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  return idioma === "aleman" ? "Guten Tag!" : idioma === "mandarin" ? "Ni Hao!" : idioma === "ingles" ? "Hello!" : "Hola!";
 }
 
 function colors(color) {
@@ -38,18 +42,38 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  var resultado = "Color not found";
+
+  switch (color) {
+    case "blue":
+      resultado = "This is blue";
+      break;
+    case "red":
+      resultado = "This is red";
+      break;
+    case "green":
+      resultado = "This is green";
+      break;
+    case "orange":
+      resultado = "This is orange";
+      break;
+  }
+
+  return resultado;
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero < 50 && numero > 20;
 }
 
 function esEntero(numero) {
@@ -60,6 +84,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return numero === Math.floor(numero);
 }
 
 function fizzBuzz(numero) {
@@ -67,15 +92,35 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  var string = "";
+  var fizz = numero % 3 === 0;
+  var buzz = numero % 5 === 0;
+  if(!fizz && !buzz){
+    return numero;
+  } else {
+    return string.concat(fizz ? "fizz" : "", buzz ? "buzz" : "");
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"                                 num1 > num2 && num3        => num1 > 0
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"                                                            num1 || num2 || num3 < 0   => "Hay negativos"
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.                                            num3 > num1 && num2        => num3 + 1
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".                                                num1 || num2 || num3 === 0 => error
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.                                                             else => false
+  var resultado = "";
+  var booleano = false;
+  if(num1 > 0 && num2 > 0 && num3 > 0 && num2 > num1 && num2 > num3){
+    booleano = true;
+  } else {
+    resultado = num1 > num2 && num1 > num3 && num1 > 0 ? "Número 1 es mayor y positivo" : resultado;
+    resultado = num1 < 0 || num2 < 0 || num3 < 0       ? "Hay negativos"                : resultado;
+    resultado = num3 > num1 && num3 > num2             ? num3 + 1                       : resultado;
+    num3 = resultado === num3 + 1 ? num3 + 1 : num3;
+    resultado = num1 === 0 || num2 === 0 || num3 === 0 ? "Error"                        : resultado;
+  }
+  return booleano ? false : resultado;
 }
 
 function esPrimo(numero) {
@@ -84,32 +129,51 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  var esPrimoInicial = numero == 2 || numero == 3 || numero == 5 || numero == 7;
+  var esDivisible = false;
+
+  if(!esPrimoInicial){
+    esDivisible = numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0;
+  }
+
+  return (esPrimoInicial || !esDivisible) && (numero > 1);
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  return valor ? "Soy verdadero" : "Soy falso";
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var tabla = [];
+  for(var i = 0; i <= 10; i++){
+    tabla.push(i * 6);
+  }
+  console.log(tabla);
+  return tabla;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  return numero > 99 && numero < 1000;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var iterador = 0;
+  do{
+    iterador += 1;
+    numero += 5;
+  } while(iterador < 8);
+  return numero;
 }
 
 
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for(var i = 0; i < array.length; i++) {
+    array[i] = array[i] + 1;
+  }
+  return array;
 }
 
 
@@ -30,6 +37,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +58,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ");
 }
 
 
@@ -54,6 +66,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return array.includes(elemento);
 }
 
 
@@ -61,6 +74,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+  return suma;
 }
 
 
@@ -68,6 +86,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < resultadosTest.length; i++) {
+    suma += resultadosTest[i];
+  }
+  return i > 0? suma / i : 0;
 }
 
 
@@ -75,20 +98,33 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max(...numeros);
 }
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto = 1;
+  if(arguments.length === 0) {
+    return 0;
+  }
+  for(var i = 0; i < arguments.length; i++) {
+    producto *= arguments[i];
+  }
+  return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador = 0;
+  for(var i = 0; i < arreglo.length; i++) {
+    contador = arreglo[i] > 18? contador + 1 : contador;
+  }
+  return contador;
 }
 
 
@@ -97,7 +133,7 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  return numeroDeDia === 1 || numeroDeDia === 7? "Es fin de semana" : "Es dia Laboral";
 } 
 
 
@@ -105,7 +141,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  return n.toString().charAt(0) === "9";
 }
 
 
@@ -113,7 +149,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var iguales = true;
+  for(var i = 0; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i + 1]) {
+      iguales = false;
+    }
+  }
+  return iguales;
 } 
 
 
@@ -122,6 +164,15 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var meses = [];
+  var checker = {Enero: false, Marzo: false, Noviembre: false};
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      meses.push(array[i]);
+      checker[array[i]] = true;
+    }
+  }
+  return checker["Enero"] && checker["Marzo"] && checker["Noviembre"]? meses : "No se encontraron los meses pedidos";
 }
 
 
@@ -129,6 +180,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var mayores = [];
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] > 100) {
+      mayores.push(array[i]);
+    }
+  }
+  return mayores;
 }
 
 
@@ -140,6 +198,14 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var resultado = [];
+  for(var i = 0; i < 10; i ++){
+    resultado.push(numero += 2);
+    if(numero === i) {
+      break;
+    }
+  }
+  return numero === i? "Se interrumpió la ejecución" : resultado;
 }
 
 
@@ -150,6 +216,14 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  resultado = [];
+  for(var i = 0; i < 10; i++) {
+    if(i === 5) {
+      continue;
+    }
+    resultado.push(numero += 2);
+  }
+  return resultado;
 }
 
 
